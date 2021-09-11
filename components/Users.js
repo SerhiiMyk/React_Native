addd98ae0d9c355eee5b8a3562e3ebfb2dffa4e2
SerhiiMyk <sergey.mykolaichuk@gmail.com>
2021-09-11 21:04:38 +0300

    init

---
+++ components/Users.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {StyleSheet} from "react-native";
+import {Text, View,Button,TouchableOpacity} from "react-native";
+
+const User= () => {
+    return <View>
+        <Text>User page</Text>
+    </View>
+};
+export default User;
+let styles = StyleSheet.create({});
